878e1b13208e943c76962b14d27545e1388d217c
tarielTitirashvili <tarieltitirashvili@gmail.com>
2022-11-22 18:19:41 +0400

    ts.config setup

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
 const message = 'string';
+const button = document.getElementById('button');
+if (button) {
+    button.addEventListener('click', () => {
+        console.log('i was clicked');
+    });
+}
+console.log(message);
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
 const message: string = 'string'
+
+const button = document.getElementById('button')
+ 
+if(button){
+  button.addEventListener('click', ()=>{
+    console.log('i was clicked')
+  })
+}
+
+console.log(message)
