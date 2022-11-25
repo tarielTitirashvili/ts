6182a08125620fe059e7c28f5fdc5949a90f936e
tarielTitirashvili <tarieltitirashvili@gmail.com>
2022-11-25 18:17:14 +0400

    generics start

--- dist/app.js
+++ dist/app.js
@@ -6,5 +6,25 @@ if (button) {
         console.log('i was clicked');
     });
 }
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('here');
+    }, 1000);
+});
+promise.then((data) => {
+    console.log(data);
+});
 console.log(message);
+function names(a, b) {
+    return Object.assign(Object.assign({}, a), b);
+}
+const mergedNames = names({ name: 'tariel' }, { age: 25 });
+mergedNames.age;
+mergedNames.name;
+function test(value) {
+    return {
+        value,
+        count: `this word has ${value.length} letter`
+    };
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,38 @@ if(button){
   button.addEventListener('click', ()=>{
     console.log('i was clicked')
   })
+} 
+
+const promise: Promise<string> = new Promise((resolve, reject) =>{
+  setTimeout(()=>{
+    console.log('here')
+  }, 1000)
+})
+
+promise.then((data)=>{
+  console.log(data)
+})
+console.log(message)
+
+
+function names<T extends object, A extends object>(a: T, b: A)  {
+  return {
+    ...a, ...b
+  }
 }
 
-console.log(message)
+const mergedNames = names({name: 'tariel'}, {age: 25})
+
+mergedNames.age
+mergedNames.name
+
+interface ILength{
+  length: number
+}
+
+function test<T extends ILength>(value: T): {value: T, count: string} {
+  return{
+    value,
+    count: `this word has ${value.length} letter`
+  }
+}
